acf694375b3c5b50012e20db1089a58344e72aab
GustZeppelin <gustzeppelin@gmail.com>
2026-03-04 21:38:54 -0300

    adicionado as habilidades e suas descrições na pagina details

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -13,18 +13,7 @@ function Details() {
 
     const [data, setData] = useState([]);
     const [descriptions, setDescriptions] = useState([]);
-
-
-    // useEffect(() => {
-
-    //   axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
-    //     .then(response => setData(response.data))
-    //     .catch(error => console.error(error));
-
-    //    axios.get(`https://pt.wikipedia.org/api/rest_v1/page/summary/${data?.name}/`)
-    //     .then(response => setDescriptions(response.data))
-    //     .catch(error => console.error(error));
-    // }, []);
+    const [abilities, setAbilities] = useState([]);
 
     useEffect(() => {
       async function fetchData() {
@@ -36,10 +25,22 @@ function Details() {
 
           const wikiResponse = await axios.get(`https://pt.wikipedia.org/api/rest_v1/page/summary/${pokemon.name}`);
           setDescriptions(wikiResponse.data);
+
+          // const abilityResponse = await axios.get(pokemon.abilities[0].ability.url);
+          // setAbility(abilityResponse.data);      
+          
+          const abilityResponses = await Promise.all(
+            pokemon.abilities.map(a => axios.get(a.ability.url))
+          );
+
+          const abilitiesData = abilityResponses.map(res => res.data);
+
+          setAbilities(abilitiesData);
+
         } catch (error) {
           console.error(error);
         }
-      }
+        }
       fetchData();
     }, []);
 
@@ -57,21 +58,35 @@ function Details() {
                       <p className="data-pokedex-item">Height: {data?.height}</p>
                       <p className="data-pokedex-item">Weight: {data?.weight}</p>
                       <p className="data-pokedex-item">Base Experience: {data?.base_experience}</p>
-                      <p>Types: {data?.types[0]?.type?.name}</p>
-                      <div className="">
-                        {/* <span className="">{data?.types[0].type.name}</span> */}
-                        {/* {data?.types?.length > 1 && (
-                        <span className="{}"> {data?.types[1]?.type?.name}</span>
-                      )} */}
-                      </div>
-                  
-                    </div>
-                    
+
+
+                      <div className="types-container">
+                        <p className="data-pokedex-item">Types:</p> 
+                        {data?.types?.map((typeInfo) => (
+                          <span key={typeInfo.type.name} className={`icon${typeInfo.type.name}`}>
+                            {typeInfo.type.name}
+                          </span>
+                        ))}
+                      </div>       
+                    </div> 
                 </div>
+                        <div className="abilities">
+                          <h1>Abilities</h1>
+                          {abilities.map((ability) => {
+                            const effect = ability.effect_entries.find(
+                              (e) => e.language.name === "en"
+                            );
+
+                            return (
+                              <div className="ability" key={ability.name}>
+                                <h2 className="ability-name">{ability.name}</h2>
+                                <span className="ability-description">{effect?.effect}</span>
+                              </div>
+                            );
+                          })}
+
+                        </div>
                 <button className="back-button" onClick={() => navigate('/')}>Back</button>
-                
-
-
             </div>
         </div>
     )
